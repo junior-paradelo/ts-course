e483413f72675ecfbbcbb83911709e240c8bff1a
Junior Paradelo <juniorparadelo@gmail.com>
2024-04-18 19:05:40 +0200

    fix: file not uploading

---
+++ src/app/products/product.dto.ts
@@ -0,0 +1,19 @@
+import { Product } from './product.model';
+
+// omit this properties
+export interface CreateProductDto
+  extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
+  categoryId: string;
+}
+
+// pick only this properties
+type examplePick = Pick<Product, 'color' | 'description'>;
+
+// partial needn't fill all fields
+export interface UpdateProductDto extends Partial<CreateProductDto> {}
+
+// required all must
+type example2 = Required<Product>;
+
+export interface FindProductDto extends Readonly<Partial<Product>> {}
+
