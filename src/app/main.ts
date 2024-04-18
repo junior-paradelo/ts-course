9e34cfa143b2b2378f54ff9ceebc5777f33941f6
Junior Paradelo <juniorparadelo@gmail.com>
2024-04-18 18:43:06 +0200

    feat: omit and pick examples to dtos

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,25 +3,16 @@ import { addProduct, products } from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
-    id: faker.string.uuid(),
     description: faker.commerce.productDescription(),
     title: faker.commerce.productName(),
     image: faker.image.url(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
     stock: faker.datatype.number({ min: 10, max: 100 }),
     color: faker.color.human(), // Corregido el método para obtener un color
     size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']), // Corregido el método para obtener un elemento aleatorio de un array
     price: parseFloat(faker.commerce.price()), // Corregido el método para obtener un precio y parsearlo a float
     isNew: faker.datatype.boolean(),
     tags: [faker.lorem.word(), faker.lorem.word(), faker.lorem.word()], // Ejemplo de tres palabras aleatorias como tags
-    category: {
-      id: faker.string.uuid(),
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-    },
+    categoryId: faker.string.uuid(),
   });
 }
-
 console.log(products);
